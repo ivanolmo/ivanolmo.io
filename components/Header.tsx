cd1c6c0a588c118199a4e17ea2a5048a69bd46df
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-20 18:46:35 -0500

    Add header component

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+function Header() {
+  return (
+    <header className='sticky top-0 z-10 flex items-center justify-between max-w-3xl px-6 py-4 mx-auto bg-slate-200 dark:bg-slate-800'>
+      <Link href='/'>
+        <a className='flex items-center gap-2 w-fit'>
+          <div className='w-12 h-12 overflow-hidden border-2 border-blue-800 rounded-full dark:border-blue-400 md:w-16 md:h-16'>
+            <Image
+              src='/static/images/header-image.webp'
+              alt='ivan'
+              width='48px'
+              height='48px'
+              layout='responsive'
+            />
+          </div>
+          <span className='text-xl font-bold text-blue-800 md:text-2xl dark:text-blue-400'>
+            ivanolmo.io
+          </span>
+        </a>
+      </Link>
+      <div className='justify-self-end'>{/**theme switcher*/}</div>
+    </header>
+  );
+}
+
+export default Header;
