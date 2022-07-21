91a5fe6ea2614c09b9e2d1dbac309b35b2c60e10
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-20 19:02:32 -0500

    Update _app layout with header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Header from '../components/Header';
+
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
