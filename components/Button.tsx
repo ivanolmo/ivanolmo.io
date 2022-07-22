602a7f198da0f28824ebc2844450c6600d3ea6d2
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-21 21:26:07 -0500

    Add utility button component

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+export default function Button(props: {
+  onClick?: () => void;
+  containerClasses: string;
+  textClasses: string;
+  buttonText: string;
+}) {
+  return (
+    <button
+      type='button'
+      onClick={() => props.onClick?.()}
+      className={`flex justify-center items-center py-4 px-6 rounded-full cursor-pointer ${props.containerClasses}`}
+    >
+      <div
+        className={`flex justify-center items-center text-xl font-bold tracking-wide ${props.textClasses}`}
+      >
+        {props.buttonText}
+      </div>
+    </button>
+  );
+}
