cf9c7fa5d7d3c2fd9595738ba14b25fbfad0081b
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-22 20:33:49 -0500

    Refactor styling
    Added hover color to header link

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ function Header() {
   return (
     <header className='sticky top-0 z-10 flex items-center justify-between max-w-3xl px-6 py-4 mx-auto bg-slate-200 dark:bg-slate-800'>
       <Link href='/'>
-        <a className='flex items-center gap-2 w-fit'>
-          <div className='w-12 h-12 overflow-hidden border-2 border-blue-800 rounded-full dark:border-blue-400 md:w-16 md:h-16'>
+        <a className='flex items-center gap-2 group w-fit'>
+          <div className='w-12 h-12 overflow-hidden border-2 border-blue-800 rounded-full group-hover:border-blue-600 dark:border-blue-400 md:w-16 md:h-16'>
             <Image
               src='/static/images/header-image.webp'
               alt='ivan'
@@ -15,7 +15,7 @@ function Header() {
               layout='responsive'
             />
           </div>
-          <span className='text-xl font-bold text-blue-800 md:text-2xl dark:text-blue-400'>
+          <span className='text-xl font-bold text-blue-800 group-hover:text-blue-600 md:text-2xl dark:text-blue-400'>
             ivanolmo.io
           </span>
         </a>
