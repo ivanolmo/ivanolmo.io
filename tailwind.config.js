330c393b9eb9ba655c80fec1365b2d603cd629b5
Ivan Olmo <ivan.olmo@gmail.com>
2022-07-24 18:03:27 -0500

    Update tailwind config and deps for theme switching
    Added next-themes package for easy theme switching

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  darkMode: 'class',
   theme: {
     fontFamily: {
       roboto: ['Roboto', 'sans-serif'],
