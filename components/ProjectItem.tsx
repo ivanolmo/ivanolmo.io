1677183c725b7d2dcbfe623a10d5b843941c6e4f
Ivan Olmo <ivan.olmo@gmail.com>
2023-03-20 13:38:59 -0500

    Refactor component
    Made image a link to the live site
    Added new headings for the 'About' and 'Tech' info on projects
    Updated prop names to match data file

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -2,37 +2,47 @@ import Image from 'next/image';
 
 import Button from './Button';
 
-function ProjectItem(props: {
+interface ProjectItemProps {
   title: string;
   image: string;
   description: string;
   tech: string;
-  repository: string;
-  link: string;
-}) {
+  repoLink: string;
+  liveLink: string;
+}
+
+function ProjectItem(props: ProjectItemProps) {
   return (
-    <div className='py-4 pl-4 space-y-6 border-l-2 sm:pl-8 border-slate-800 dark:border-slate-200 rounded-l-2xl'>
+    <div className='p-4 space-y-6 border-4 sm:p-8 border-slate-900 rounded-2xl'>
       <h3>{props.title}</h3>
       <div className='w-auto h-auto overflow-hidden rounded-xl'>
-        <Image
-          src={`/static/images/${props.image}`}
-          alt={props.title}
-          width='100%'
-          height='100%'
-          layout='responsive'
-        />
+        <a href={props.liveLink} target='_blank' rel='noreferrer'>
+          <Image
+            src={`/static/images/${props.image}`}
+            alt={props.title}
+            width='100%'
+            height='100%'
+            layout='responsive'
+          />
+        </a>
       </div>
-      <p>{props.description}</p>
-      <p>{props.tech}</p>
+      <div className='space-y-2'>
+        <h4>About this project:</h4>
+        <p>{props.description}</p>
+      </div>
+      <div className='space-y-2'>
+        <h4>Tech used:</h4>
+        <p>{props.tech}</p>
+      </div>
       <div className='grid gap-4 sm:grid-cols-2'>
-        <a href={props.link} target='_blank' rel='noreferrer'>
+        <a href={props.liveLink} target='_blank' rel='noreferrer'>
           <Button
             containerClasses='bg-blue-800 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-400 w-full col-span-1'
             textClasses='text-white'
             buttonText='Live Site'
           />
         </a>
-        <a href={props.repository} target='_blank' rel='noreferrer'>
+        <a href={props.repoLink} target='_blank' rel='noreferrer'>
           <Button
             containerClasses='bg-blue-800 dark:bg-blue-600 hover:bg-blue-600 dark:hover:bg-blue-400 w-full col-span-1'
             textClasses='text-white'
