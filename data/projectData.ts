d43ef40242f274568f404335eea718d9cbac96d9
Ivan Olmo <ivan.olmo@gmail.com>
2023-03-20 13:38:02 -0500

    Add new file for all project data

---
+++ data/projectData.ts
@@ -0,0 +1,40 @@
+export const projectData = [
+  {
+    title: 'Kanban Task Manager',
+    image: 'kanban.webp',
+    description:
+      'I built this full stack app to have a task manager that I could use to organize my work. It is a Kanban board that allows users to create, edit, and delete tasks. Users can create different boards, and each board can have any number of columns. Tasks can have subtasks as well. The app is responsive and supports both light and dark themes.',
+    tech: 'React, Next.js, TypeScript, tRPC, React Query, Zustand, NextAuth, MongoDB, Tailwind.',
+    repoLink: 'https://github.com/ivanolmo/kanban-task-management',
+    liveLink: 'https://nextjs-kanban.vercel.app/',
+  },
+  {
+    title: 'Invoice App',
+    image: 'invoices.webp',
+    description:
+      'This full stack app allows users to create an account and manage invoices. Form data is validated, and the invoice form can be partially completed and saved as a draft. Data is updated with React hooks and/or Next.js API requests. This app is responsive and supports both light and dark themes, and users can delete their account and data from the profile page.',
+    tech: 'React, Next.js, Formik, Google Firebase (Authentication, Firestore, and Functions), Tailwind.',
+    repoLink: 'https://github.com/ivanolmo/nextjs-invoice-app',
+    liveLink: 'https://nextjs-invoice-app.vercel.app/',
+  },
+  {
+    title: 'Designo Multi-Page Website',
+    image: 'designo.webp',
+    description:
+      'This multi-page site was built using the Astro framework. I used Astro because this site is mostly static and this tool is perfect for that. It also allows the flexibility of using bits and pieces of a variety of UI frameworks when needed, and I used React for a few components. The site also includes live maps of fictional office locations powered by Leaflet.js.',
+    tech: 'Astro, React, TypeScript, Leaflet.js, Tailwind.',
+    repoLink: 'https://github.com/ivanolmo/designo-site',
+    liveLink: 'https://designo-site-ivan.vercel.app/',
+  },
+  ,
+  {
+    title: 'Space Tourism',
+    image: 'space.webp',
+    description:
+      'A cool, simple site I built using HTML, CSS, and JavaScript. Accessibility and a mobile-first workflow were prioritized. The site has multiple pages, each displaying dynamic data that is fetched from a local data file. The data is updated based on user interactions. The site utilizes custom CSS properties and variables, is fully responsive, and has an animated mobile menu.',
+    tech: 'HTML, CSS, and JavaScript.',
+    repoLink:
+      'https://github.com/ivanolmo/frontend-mentor-projects/tree/main/space-tourism-website-main',
+    liveLink: 'https://space-tourism-ivan.netlify.app/',
+  },
+];
