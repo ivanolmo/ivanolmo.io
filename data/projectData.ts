064e7063bdccf0b4c569ef5ddec6c1d87b018971
Ivan Olmo <48425752+ivanolmo@users.noreply.github.com>
2024-05-09 15:45:15 -0700

    Update project links

--- data/projectData.ts
+++ data/projectData.ts
@@ -15,7 +15,7 @@ export const projectData = [
       'This full stack app allows users to create an account and manage invoices. Form data is validated, and the invoice form can be partially completed and saved as a draft. Data is updated with React hooks and/or Next.js API requests. This app is responsive and supports both light and dark themes, and users can delete their account and data from the profile page.',
     tech: 'React, Next.js, Formik, Google Firebase (Authentication, Firestore, and Functions), Tailwind.',
     repoLink: 'https://github.com/ivanolmo/nextjs-invoice-app',
-    liveLink: 'https://nextjs-invoice-app.vercel.app/',
+    liveLink: 'https://invoices.ivanolmo.com/',
   },
   {
     title: 'Designo Multi-Page Website',
@@ -24,17 +24,6 @@ export const projectData = [
       'This multi-page site was built using the Astro framework. I used Astro because this site is mostly static and this tool is perfect for that. It also allows the flexibility of using bits and pieces of a variety of UI frameworks when needed, and I used React for a few components. The site also includes live maps of fictional office locations powered by Leaflet.js.',
     tech: 'Astro, React, TypeScript, Leaflet.js, Tailwind.',
     repoLink: 'https://github.com/ivanolmo/designo-site',
-    liveLink: 'https://designo-site-ivan.vercel.app/',
-  },
-  ,
-  {
-    title: 'Space Tourism',
-    image: 'space.webp',
-    description:
-      'A cool, simple site I built using HTML, CSS, and JavaScript. Accessibility and a mobile-first workflow were prioritized. The site has multiple pages, each displaying dynamic data that is fetched from a local data file. The data is updated based on user interactions. The site utilizes custom CSS properties and variables, is fully responsive, and has an animated mobile menu.',
-    tech: 'HTML, CSS, and JavaScript.',
-    repoLink:
-      'https://github.com/ivanolmo/frontend-mentor-projects/tree/main/space-tourism-website-main',
-    liveLink: 'https://space-tourism-ivan.netlify.app/',
+    liveLink: 'https://designo.ivanolmo.com/',
   },
 ];
